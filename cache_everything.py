b12e4c2bdf60aabd4eb5359958f31f5e668e08d1
Andrew Turner <andrew.turner@ga.gov.au>
2018-11-12 13:52:25 +1100

    Changed ground_grav_points and aem_lines back to original

--- cache_everything.py
+++ cache_everything.py
@@ -119,7 +119,8 @@ def main():
                                                                              )
 
                 print('\tCaching image {} from {}'.format(image_basename, wms_url))
-                s3_key_name = re.sub('\tmp', '', cache_dir)
+                s3_key_name = re.sub('/tmp', '', cache_dir)
+                #s3_key_name = re.sub('?image=', '', s3_key_name)
                 print("s3_key_name" + str(s3_key_name))
                 cached_image_url_path = cache_image_file(dataset_type, image_basename, wms_url, s3_bucket_name, s3_key_name)
 

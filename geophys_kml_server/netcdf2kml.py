910e527564c7cd4556ffeb59d5b38b5b77975c1c
Andrew Turner <andrew.turner@ga.gov.au>
2018-11-15 13:51:49 +1100

    Removed makedirs

--- geophys_kml_server/netcdf2kml.py
+++ geophys_kml_server/netcdf2kml.py
@@ -99,14 +99,16 @@ class NetCDF2kmlConverter(object):
         #                   'kml_server_cache',
         #                   dataset_type
         #                   )
-        self.cache_dir = os.path.join(settings['global_settings'].get('cache_root_dir'), 'kml_server_cache/', dataset_type, dataset_metadata_dict['netcdf_basename'])
+        self.cache_dir = os.path.join(settings['global_settings'].get('cache_root_dir'), 'kml_server_cache', dataset_type, dataset_metadata_dict['netcdf_basename'])
         logger.debug("cached_dir: " + str(self.cache_dir))
 
-        try:
-            original_umask = os.umask(0)
-            os.makedirs(self.cache_dir, mode=0o777, exist_ok=True)
-        finally:
-            os.umask(original_umask)
+        #os.makedirs(self.cache_dir, exist_ok=True)
+
+        # try:
+        #     original_umask = os.umask(0)
+        #     os.makedirs(self.cache_dir, mode=0777, exist_ok=True)
+        # finally:
+        #     os.umask(original_umask)
 
         
         logger.debug('Instantiating NetCDF2kmlConverter object for {} datasets'.format(dataset_type))
